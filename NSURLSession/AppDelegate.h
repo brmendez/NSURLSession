9676014d881ea5ffc2c84140878c0de759812a34
Brian Mendez <brmendez@gmail.com>
2014-11-21 21:49:35 -0800

    Initial Commit

---
+++ NSURLSession/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NSURLSession
+//
+//  Created by Brian Mendez on 11/21/14.
+//  Copyright (c) 2014 Brian Mendez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
